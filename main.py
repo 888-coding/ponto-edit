d9bcafe982e65e1096907d525348ec00cdd27ed2
888-coding <pachelee@gmail.com>
2025-05-25 11:42:26 -0300

    改為中文comment

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from datetime import time
 import colorama 
 from colorama import Fore 
 
-
-#01: Procurar o arquivo 
+# 程式開始, 尋找 excel 檔案, 檔案位子
 nome_arquivo = "file.xlsx"
-#01.01 Diretorio do programa 
 diretorio_atual = os.path.dirname(os.path.realpath(__file__))
 
-#01.02 Abrir arquivo 
+# 開啟檔案 , 開啟列表
 wb = openpyxl.load_workbook(nome_arquivo)
-#01.03 Abrir nomes das planilhas.
 todas_planilhas = wb.sheetnames
-#01.END Arquivo encontrado 
 print(Fore.GREEN + "Bem vindo ao sistema" + Fore.RESET)
-#02: Alterar data
+
+# 改日期
 desejaAlterarData = input(Fore.YELLOW + "Deseja corrigir a o mes e ano ? (S/N)  : " + Fore.RESET).upper()
 if desejaAlterarData == "S":
 	novoMes = int(input("Favor digite o mês : "))
@@ -29,25 +26,28 @@ if desejaAlterarData == "S":
 	print(Fore.GREEN + "Gravado com sucesso, abra de novo para alterar." + Fore.RESET)
 
 
-#B2 插入 時間 小時 和 分鐘 
+#B2 插入時間(小時和分鐘) 
 #sheet = arquivo['PEDRO']
 #sheet['B2'].value = time(9,00,00)
 #arquivo.save(nome_arquivo)
 
-#03: Procurar a planilha do nome 
+# 尋找名字 
 sheetNome = input("Nome: ").upper()
 
 nome_planilhas = wb.sheetnames
 
 if sheetNome in nome_planilhas:
+	# 找到了名字
 	print(Fore.GREEN + "Encontrado" + Fore.RESET)
 	sh = wb[sheetNome]
 	for linha in range(2, 33):
-		#lista = ['B', 'C', 'D', 'E']
+		# 列表分為4行: B,C,D,E 
+		# 分別為 上班時間, 吃飯時間, 吃飯回來時間, 下班時間
+		# lista = ['B', 'C', 'D', 'E']
 		campo = 'A' + str(linha)
 		print(f"Dia : {sh[campo].value}")
 		print("**Entrada")
-		# Campo de Entrada : Horas 
+		# 進來時間 (小時) 
 		while True: 
 			horas01 = input("Horas : ")
 			if horas01.isnumeric():
@@ -57,7 +57,7 @@ if sheetNome in nome_planilhas:
 					print(Fore.RED + "Erro!" + Fore.RESET)
 			else:
 				print(Fore.RED + "Erro!" + Fore.RESET)
-		# Campo de Entrada : Minutos 
+		# 進來時間 (分鐘) 
 		while True:
 			minutos01 = input("Minutos : ")
 			if minutos01.isnumeric():
@@ -68,7 +68,7 @@ if sheetNome in nome_planilhas:
 			else:
 				print(Fore.RED + "Erro!" + Fore.RESET)
 		print("**Almoço")
-		# Campo de Almoço : Horas
+		# 吃飯時間 (小時)
 		while True:
 			horas02 = input("Horas : ")
 			if horas02.isnumeric():
@@ -78,7 +78,7 @@ if sheetNome in nome_planilhas:
 					print(Fore.RED + "Erro!" + Fore.RESET)
 			else:
 				print(Fore.RED + "Erro!" + Fore.RESET)
-		# Campo de Almoço : Minutos 
+		# 吃飯時間 (分鐘) 
 		while True:
 			minutos02 = input("Minutos : ")
 			if minutos02.isnumeric():
@@ -89,7 +89,7 @@ if sheetNome in nome_planilhas:
 			else:
 				print(Fore.RED + "Erro!" + Fore.RESET)
 		print("**Volta almoço")
-		# Campo de Volta de almoço : Horas
+		# 吃飯回來時間 (小時)
 		while True:
 			horas03 = input("Horas : ")
 			if horas03.isnumeric():
@@ -99,7 +99,7 @@ if sheetNome in nome_planilhas:
 					print(Fore.RED + "Erro!" + Fore.RESET)
 			else:
 				print(Fore.RED + "Erro!" + Fore.RESET)
-		# Campo de Volta de almoço : Minutos 
+		# 吃飯回來時間 (分鐘) 
 		while True:
 			minutos03 = input("Minutos : ")
 			if minutos03.isnumeric():
@@ -110,7 +110,7 @@ if sheetNome in nome_planilhas:
 			else:
 				print(Fore.RED + "Erro!" + Fore.RESET)
 		print("**Saída")
-		# Campo de Saída : Horas 
+		# 下班時間 (小時) 
 		while True:
 			horas04 = input("Horas : ")
 			if horas04.isnumeric():
@@ -120,7 +120,7 @@ if sheetNome in nome_planilhas:
 					print(Fore.RED + "Erro!" + Fore.RESET)
 			else:
 				print(Fore.RED + "Erro!" + Fore.RESET)
-		# Campo de Saída : Minutos
+		# 下班時間 (分鐘)
 		while True:
 			minutos04 = input("Minutos : ")
 			if minutos04.isnumeric():
@@ -143,4 +143,5 @@ if sheetNome in nome_planilhas:
 			sh[campo].value = time(int(horas04),int(minutos04),00)
 			wb.save(nome_arquivo)
 else:
+	#沒有找到名字
 	print(Fore.RED + "Nome errado" + Fore.RESET)
